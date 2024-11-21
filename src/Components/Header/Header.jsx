ccf3522f9aae38137e4956376fb2f9242c9a76b7
unknown <37395@fiecalunos.lan>
2024-11-21 20:27:48 -0300

    primeiro commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Nome do negócio</h1>
+        <a href="/">Cadastro</a>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Produtos</a>
+            <a href="">Sobre</a>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
