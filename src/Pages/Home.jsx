59dd4fbbf3b1d4ccd32f3aabc4df59ab30a8cb1f
MagalDevs <danielgalfla12@gmail.com>
2024-11-23 14:28:17 -0300

    commit corrigido

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import'./Styles/Home.css'
+import Navbar from '../Components/Navbar/Navbar'
 
 const Home = () => {
   return (
     <>
+    <Navbar/>
     <h1>Bem vindo ao xxxxxx</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias quis alias, odit quo voluptatem deleniti soluta eum dolores accusantium. Quaerat, vero adipisci? Inventore dicta ipsa mollitia, dolorem in minus porro ea temporibus. Voluptate, atque! Doloremque exercitationem quia provident fugit rerum commodi cupiditate, cumque impedit doloribus sequi, non atque repudiandae magni. Consequuntur voluptatum nihil saepe eos nobis vitae nostrum error neque nulla quas magnam dignissimos nisi impedit accusantium repellendus ratione illum eaque distinctio, eum odit obcaecati dicta ad ea accusamus! Placeat officiis expedita harum accusamus labore fuga architecto praesentium ipsum natus molestias amet saepe ex corrupti vel rerum animi, laudantium nostrum!</p>
     <div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Produto from './Pages/Produto.jsx'
+import App from './App'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Produto/>
+    <App/>
   </StrictMode>,
 )
